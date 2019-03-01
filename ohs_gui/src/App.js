a7f988045d9fbdff80bddb28bfb1bfe4db510490
Alec Gibson <violinmonkey42@gmail.com>
2019-03-01 12:29:08 -0500

    added react router, and courses page

--- ohs_gui/src/App.js
+++ ohs_gui/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import OfficeHours from "./components/OfficeHour"
+import Course from "./components/Course"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <OfficeHours slotNum={5}> </OfficeHours>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="nav">
+            <div className="links">
+              <Link to="/course">
+                COURSES
+            </Link>
+              <Link to="/">
+                HOME
+            </Link>
+            </div>
+          </div>
+          <header className="App-header">
+            <Switch>
+              <Route exact path="/" render={() => <OfficeHours slotNum={5}> </OfficeHours>} />
+              <Route exact path="/course" render={() => <Course />} />
+            </Switch>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
---
+++ ohs_gui/src/components/Course.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Course.css';
+
+class OfficeHour extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>THIS IS A COURSE</h1>
+      </div>
+    );
+  }
+}
+
+export default OfficeHour;
