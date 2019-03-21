9d10da8359b60e84967b46ba0cdd138b41c4d69b
chaitanyavp <chaitanya1998@gmail.com>
2019-03-21 14:36:29 -0400

    add new sql tables for meeting models

--- backend/core/table_setup.sql
+++ backend/core/table_setup.sql
@@ -32,3 +32,43 @@ CREATE TABLE sections(
     REFERENCES instructors (user_name) MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+CREATE TABLE meetings (
+ meeting_id VARCHAR (50) PRIMARY KEY,
+ instructor VARCHAR (50) NOT NULL,
+ student VARCHAR (50) NOT NULL,
+ start_time VARCHAR (50) NOT NULL,
+ end_time VARCHAR (50) NOT NULL,
+ CONSTRAINT instructor_fkey FOREIGN KEY (instructor)
+    REFERENCES instructors (user_name) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+ CONSTRAINT student_fkey FOREIGN KEY (student)
+    REFERENCES students (student_number) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE notes (
+ note_id VARCHAR (50) PRIMARY KEY,
+ meeting_id VARCHAR (50) NOT NULL,
+ time_stamp VARCHAR (50) NOT NULL,
+ content_text VARCHAR (50),
+ CONSTRAINT meeting_fkey FOREIGN KEY (meeting_id)
+    REFERENCES meetings (meeting_id) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION 
+);
+
+CREATE TABLE comments (
+ comment_id VARCHAR (50) PRIMARY KEY,
+ meeting_id VARCHAR (50) NOT NULL,
+ author_if_instructor VARCHAR (50),
+ author_if_student VARCHAR (50),
+ time_stamp VARCHAR (50) NOT NULL,
+ content_text VARCHAR (50),
+ CONSTRAINT meeting_fkey FOREIGN KEY (meeting_id)
+    REFERENCES meetings (meeting_id) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+ CONSTRAINT has_author CHECK (
+      CASE WHEN author_if_instructor IS NULL THEN 0 ELSE 1 END +
+      CASE WHEN author_if_student  IS NULL THEN 0 ELSE 1 END = 1
+    )
+);
